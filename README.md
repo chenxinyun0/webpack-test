4209e134a4bd8c5da4880c86ecd2a43909702f9b
chenxinyun0 <45486541+chenxinyun0@users.noreply.github.com>
2022-09-09 17:32:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-webpack-js-org-7syetv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/webpack-webpack-js-org-7syetv)
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV === 'production';
+
+console.log(process.env.NODE_ENV);
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.svg$/i,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ filename: () => 'wwww.html' }),
+    new MiniCssExtractPlugin(),
+  ],
+};
